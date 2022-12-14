956520e80aa64256f35fad79db81301604a72bfa
Dinoosawruss <m.dormand@sky.com>
2022-12-14 19:00:55 +0000

    Day 6 Problem 2: Tuning Trouble - 1st Solution

--- 6-tuning_trouble/tuning_trouble_1.py
+++ 6-tuning_trouble/tuning_trouble_1.py
@@ -11,37 +11,60 @@ def read_input() -> str:
     with open("6-tuning_trouble/input.txt", "r", encoding="utf-8") as input_file:
         return input_file.read()
 
+def find_start_of_x(recieved_data: str, unique_chars_required: int) -> int:
+    """Find the start of a marker in a string based on x unique chars
+
+    Args:
+        recieved_data (str): The raw input data
+        unique_chars_required (int): The under of unique characters required to be correct
+
+    Returns:
+        int: The index of the final item in the location found
+    """
+    window: list = [None, None, None, None]
+
+    for start_window_index in range(len(recieved_data)-unique_chars_required):
+        window = [
+            recieved_data[start_window_index+index] for index in range(unique_chars_required)
+        ]
+
+        unique: set = set(window)
+
+        if len(unique) == unique_chars_required:
+            return start_window_index+unique_chars_required # Final value
+
+    return -1 # Couldn't find any
+
 def find_start_of_packet(recieved_data: str) -> int:
-    """Find the first group of 4 non-repeating characters (and thus start of packet)
+    """Find the start of a packet (4 unique chars in a row)
 
     Args:
         recieved_data (str): The raw input data
 
     Returns:
-        int: The index of the last item in the group of 4 non-repeating characters
+        int: The index of the last item in the start of the packet flag
     """
-    window: list = [None, None, None, None]
+    return find_start_of_x(recieved_data, 4)
 
-    for start_window_index in range(len(recieved_data)-4):
-        window = [
-            recieved_data[start_window_index],
-            recieved_data[start_window_index+1],
-            recieved_data[start_window_index+2],
-            recieved_data[start_window_index+3]
-        ]
+def find_start_of_message_marker(recieved_data: str) -> int:
+    """Find the start of a message marker (14 unique chars in a row)
 
-        unique: set = set(window)
+    Args:
+        recieved_data (str): The raw input data
 
-        if len(unique) == 4:
-            return start_window_index+4 # Final value
-
-    return -1 # Couldn't find any
+    Returns:
+        int: The index of the last item in the start of message marker flag
+    """
+    return find_start_of_x(recieved_data, 14)
 
 def main():
     """Main function - do logic
     """
-    start_index: int = find_start_of_packet(read_input())
-    print(start_index)
+    start_packet_index: int = find_start_of_packet(read_input())
+    print(start_packet_index)
+
+    start_of_message_marker: int = find_start_of_message_marker(read_input())
+    print(start_of_message_marker)
 
 if __name__ == "__main__":
     main()
